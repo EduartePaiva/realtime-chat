e29cff2a362d0a2cd215f3e388ad093ff810f4de
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2025-11-18 12:12:52 -0300

    creating auth route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,21 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import authRoute from "./routes/auth.route.js";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.route("/api/auth", authRoute);
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 5001,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
