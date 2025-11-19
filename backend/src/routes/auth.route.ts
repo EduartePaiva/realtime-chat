e1ad4c344c714758defe83d937f894049722ddb1
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2025-11-19 19:08:47 -0300

    building login api

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -7,6 +7,8 @@ import { generateJwtToken, saveJwtCookie } from "../lib/utils.js";
 
 const authRoutes = new Hono();
 
+const JWT_SECRET = process.env.JWT_SECRET!;
+
 const signupSchema = z.object({
   fullName: z.string().min(1),
   email: z.email(),
@@ -27,7 +29,7 @@ authRoutes.post("/signup", zValidator("json", signupSchema), async (c) => {
     const newUser = new User({ password: hashedPass, fullName, email });
     await newUser.save();
 
-    const jwtToken = generateJwtToken(newUser._id.toHexString(), process.env.JWT_SECRET!);
+    const jwtToken = generateJwtToken(newUser._id.toHexString(), JWT_SECRET);
     saveJwtCookie(c, jwtToken);
 
     return c.json(
@@ -45,8 +47,35 @@ authRoutes.post("/signup", zValidator("json", signupSchema), async (c) => {
     return c.json({ message: "internal server error" }, 500);
   }
 });
-authRoutes.post("/login", (c) => {
-  return c.text("login route");
+
+const loginSchema = z.object({
+  email: z.email(),
+  password: z.string().min(6),
+});
+
+authRoutes.post("/login", zValidator("json", loginSchema), async (c) => {
+  const { email, password } = c.req.valid("json");
+
+  try {
+    const user = await User.findOne({ email: email }).exec();
+    if (user === null) {
+      return c.json({ message: "Invalid email or password" }, 401);
+    }
+
+    const isPasswordRight = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordRight) {
+      return c.json({ message: "Invalid email or password" }, 401);
+    }
+
+    const jwtToken = generateJwtToken(user.id, JWT_SECRET);
+
+    return c.json({ token: jwtToken }, 201);
+  } catch (error) {
+    console.error(error);
+
+    return c.json({ message: "Internal server error" }, 500);
+  }
 });
 authRoutes.post("/logout", (c) => {
   return c.text("logout route");
