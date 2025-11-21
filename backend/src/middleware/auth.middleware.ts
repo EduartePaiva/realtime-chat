0b5f69cae67765e8284303f557aab409cb438ded
eduarte <74308809+EduartePaiva@users.noreply.github.com>
2025-11-21 15:32:04 -0300

    changed auth middleware and added env example

--- backend/src/middleware/auth.middleware.ts
+++ backend/src/middleware/auth.middleware.ts
@@ -1,10 +1,18 @@
 import { getCookie } from "hono/cookie";
 import { createMiddleware } from "hono/factory";
 import { verifyAndExtractJwtUserID } from "../lib/utils.js";
+import User from "../models/user.model.js";
+
+type UserMiddlewareData = {
+  userID: string;
+  email: string;
+  fullName: string;
+  profilePic: string;
+};
 
 export const protectRoute = createMiddleware<{
   Variables: {
-    userID: string;
+    user: UserMiddlewareData;
   };
 }>(async (c, next) => {
   const jwtToken = getCookie(c, "token");
@@ -15,7 +23,17 @@ export const protectRoute = createMiddleware<{
   try {
     const userId = verifyAndExtractJwtUserID(jwtToken, process.env.JWT_SECRET!);
 
-    c.set("userID", userId);
+    const user = await User.findById(userId).select("-password").exec();
+    if (!user) {
+      return c.json({ message: "User not found" }, 404);
+    }
+
+    c.set("user", {
+      userID: userId,
+      email: user.email,
+      fullName: user.fullName,
+      profilePic: user.profilePic,
+    });
   } catch (err) {
     console.log("error verifying token", err);
     return c.json({ message: "Unauthorized" }, 401);
